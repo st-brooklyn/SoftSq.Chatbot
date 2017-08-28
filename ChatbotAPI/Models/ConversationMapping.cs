8633361e04411a86e3e8f3656dcc0008605e8aa4
Brooklyn Steinfeld <reasontoalive@hotmail.com>
2017-08-28 14:41:20 +0700

    Add project files.

---
+++ ChatbotAPI/Models/ConversationMapping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftSq.ChatbotApi.Models
+{
+    public class ConversationMapping
+    {
+        
+        public long Id { get; set; }
+        public string RoomId { get; set; }
+        public string SenderId { get; set; }
+        public string ConversationToken { get; set; }
+    }
+}
---
+++ ChatbotAPI/Models/Messages/LineMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftSq.ChatbotApi.Models.Messages
+{
+    public class LineMessage
+    {
+        public List<Event> events { get; set; }
+    }
+
+    public class Event
+    {
+        public string replyToken { get; set; }
+        public string type { get; set; }
+        public long timestamp { get; set; }
+        public Source source { get; set; }
+        public Message message { get; set; }
+    }
+
+    public class Source
+    {
+        public string type { get; set; }
+        public string userId { get; set; }
+    }
+
+    public class Message
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string text { get; set; }
+    }
+}
